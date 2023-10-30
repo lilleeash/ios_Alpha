49a25593276a4cdf2bc8ffc110de2438adfe9a01
Darya Zhitova <lilleeash@icloud.com>
2023-10-30 14:08:40 +0300

    Create networking layer

---
+++ ios_Alpha/HTTPClient/APIMethod.swift
@@ -0,0 +1,16 @@
+//
+//  APIMethod.swift
+//  Bazar
+//
+//  Created by Darya Zhitova on 22.03.2023.
+//
+
+import Foundation
+
+enum APIMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
