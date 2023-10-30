a4d3027f15641ef25c5a54a8bb81e550ed0b05e9
Darya Zhitova <lilleeash@icloud.com>
2023-10-30 17:39:58 +0300

    Refactor beer model

--- ios_Alpha/Modules/Beers/Model/BeerModel.swift
+++ ios_Alpha/Modules/Beers/Model/BeerModel.swift
@@ -7,110 +7,18 @@
 
 import Foundation
 
-typealias BeersModel = [Temperature]
+typealias BeersModel = [BeerModel]
 
-struct Temperature: Codable {
+struct BeerModel: Decodable {
     let id: Int
-    let name, tagline, firstBrewed, description: String
-    let imageURL: String
-    let abv: Double
-    let ibu: Double?
-    let targetFg: Int
-    let targetOg: Double
-    let ebc: Int?
-    let srm, ph: Double?
-    let attenuationLevel: Double
-    let volume, boilVolume: BoilVolume
-    let method: Method
-    let ingredients: Ingredients
-    let foodPairing: [String]
-    let brewersTips: String
-    let contributedBy: ContributedBy
-
+    let name, tagline, description: String
+    let imageURL: URL
+    
     enum CodingKeys: String, CodingKey {
-        case id, name, tagline
-        case firstBrewed = "first_brewed"
+        case id
+        case name
+        case tagline
         case description
         case imageURL = "image_url"
-        case abv, ibu
-        case targetFg = "target_fg"
-        case targetOg = "target_og"
-        case ebc, srm, ph
-        case attenuationLevel = "attenuation_level"
-        case volume
-        case boilVolume = "boil_volume"
-        case method, ingredients
-        case foodPairing = "food_pairing"
-        case brewersTips = "brewers_tips"
-        case contributedBy = "contributed_by"
     }
-}
-
-struct BoilVolume: Codable {
-    let value: Double
-    let unit: Unit
-}
-
-enum Unit: String, Codable {
-    case celsius = "celsius"
-    case grams = "grams"
-    case kilograms = "kilograms"
-    case litres = "litres"
-}
-
-enum ContributedBy: String, Codable {
-    case aliSkinnerAliSkinner = "Ali Skinner <AliSkinner>"
-    case samMasonSamjbmason = "Sam Mason <samjbmason>"
-}
-
-struct Ingredients: Codable {
-    let malt: [Malt]
-    let hops: [Hop]
-    let yeast: String
-}
-
-struct Hop: Codable {
-    let name: String
-    let amount: BoilVolume
-    let add: Add
-    let attribute: Attribute
-}
-
-enum Add: String, Codable {
-    case dryHop = "dry hop"
-    case end = "end"
-    case middle = "middle"
-    case start = "start"
-}
-
-enum Attribute: String, Codable {
-    case aroma = "aroma"
-    case attributeFlavour = "Flavour"
-    case bitter = "bitter"
-    case flavour = "flavour"
-}
-
-struct Malt: Codable {
-    let name: String
-    let amount: BoilVolume
-}
-
-struct Method: Codable {
-    let mashTemp: [MashTemp]
-    let fermentation: Fermentation
-    let twist: String?
-
-    enum CodingKeys: String, CodingKey {
-        case mashTemp = "mash_temp"
-        case fermentation, twist
-    }
-}
-
-struct Fermentation: Codable {
-    let temp: BoilVolume
-}
-
-struct MashTemp: Codable {
-    let temp: BoilVolume
-    let duration: Int?
 }
