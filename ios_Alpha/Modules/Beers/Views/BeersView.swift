7feeee8f3670cca219360259fff48e2e95125a31
Darya Zhitova <lilleeash@icloud.com>
2023-10-30 17:50:36 +0300

    create beers uiview

--- ios_Alpha/Modules/Beers/Views/BeersView.swift
+++ ios_Alpha/Modules/Beers/Views/BeersView.swift
@@ -5,4 +5,34 @@
 //  Created by Darya Zhitova on 23.10.2023.
 //
 
-import Foundation
+import UIKit
+
+final class BeersUIView: UIView {
+    private lazy var tableView: UITableView = {
+        let table = UITableView()
+        table.backgroundColor = .cyan
+        return table
+    }()
+    
+    init(dataSource: UITableViewDataSource) {
+        super.init(frame: .zero)
+        backgroundColor = .white
+        self.tableView.dataSource = dataSource
+        setUpConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("BeersUIView couldn`t init")
+    }
+    
+    private func setUpConstraints() {
+        addSubview(tableView)
+        
+        NSLayoutConstraint.autoresizingMask([
+            tableView.leftAnchor.constraint(equalTo: safeAreaLayoutGuide.leftAnchor),
+            tableView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
+            tableView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor)
+        ])
+    }
+}
